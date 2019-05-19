b4e1ed62bd71a96bf2ce46ad6241a3bb1fe64f27
smart-programmer <albrns1123488@outlook.com>
2019-05-19 14:36:16 +0300

    writing s3 logic

--- WEBSITE/routes.py
+++ WEBSITE/routes.py
@@ -127,7 +127,7 @@ def uploadImage():
         description = form.description.data
         category = form.filters.data
         url = form.url.data
-        image_string = save_image(form.image.data, "static/posts/images")
+        image_string = save_image_online(form.image.data, ) # save_image(form.image.data, "static/posts/images")
 
         post = Post(image_string=image_string, category=category, post_title=title,
         post_description=description, project_link=url)
